f6c18e1e2af657c5cc8af3cec51de4a48b2e67aa
Rick Lansky <rlansky@rallydev.com>
2013-08-13 15:40:11 -0600

    updated some pages (mostly popup editor type pages) to ignore during the processing

--- background.js
+++ background.js
@@ -1,11 +1,14 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (isRallyUrl(tab.url) && changeInfo.status === 'complete') {
+    if (isLinkableRallyUrl(tab.url) && changeInfo.status === 'complete') {
         //  Trigger the event that causes us to check the page
         chrome.tabs.sendMessage(tabId, {tabUpdated: true});
     }
 });
 
-function isRallyUrl(url) {
-    return  url.indexOf('http://localhost:7001') === 0 ||
-            url.indexOf('.rallydev.com/') !== -1;
+function isLinkableRallyUrl(url) {
+    return  (url.indexOf('http://localhost:7001') === 0 ||
+             url.indexOf('.rallydev.com/') !== -1) &&
+            url.indexOf('edit.sp') == -1 &&
+            url.indexOf('new.sp') == -1 &&
+            url.indexOf('copy.sp') == -1;
 }
