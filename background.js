047b47b37eb346d379a87d9e5a22e3f171814886
Rick Lansky <rlansky@rallydev.com>
2013-08-16 09:35:28 -0600

    removed 'P' from the list of default prefixes

--- background.js
+++ background.js
@@ -11,7 +11,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 function getStoredPrefixes() {
     var storedResults = localStorage['rally_artifact_linker_prefixes'];
     if (!storedResults) {
-        storedResults = "US\nDE\nTA\nTC\nDS\nF\nP\nT\nI"
+        storedResults = "US\nDE\nTA\nTC\nDS\nF\nT\nI"
     }
     return storedResults.split("\n");
 }
